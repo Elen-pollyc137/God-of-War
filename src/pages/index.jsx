2e3057be4b54f37b37ca14f9ffbab2efeb30a1fd
Polly <poliana_elen@hotmail.com>
2022-12-10 00:23:47 -0300

    add: New Project

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import HeaderHero from "../styles-page/home/HeaderHero";
+import NavBar from "../styles-page/home/NavBar";
+
+export default function Home() {
+  return (
+    <>
+      <NavBar />
+      <HeaderHero />
+    </>
+  );
+}
---
+++ src/styles-page/home/NavBar/index.jsx
@@ -0,0 +1,44 @@
+import styles from "./styles.module.scss";
+import Link from "next/link";
+
+export default function NavBar() {
+  return (
+    <section className={styles.container}>
+      <div className={styles.box}>
+        <div className={styles.box_icon}>
+          <a href="#">
+            <picture>
+              <img src="Assets/navbar/image01.png" alt="" />
+            </picture>
+          </a>
+        </div>
+        <div className={styles.box_card}>
+          {" "}
+          <a href="#">
+            <picture>
+              <img
+                src="Assets/navbar/image02.png"
+                className={styles.img}
+                alt=""
+              />
+            </picture>
+          </a>
+        </div>
+        <div className={styles.box_menu}>
+          <Link href="#" className={styles.options}>
+            Vídeos e Telas
+          </Link>
+          <Link href="#" className={styles.options}>
+            Bastidores
+          </Link>
+          <Link href="#" className={styles.options}>
+            Comunidade
+          </Link>
+          <Link href="#" className={styles.button}>
+            Compre Agora
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+}
